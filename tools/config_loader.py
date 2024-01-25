6bf326d2c2061baecd5e7af2a6f8f02f8f327dfa
yaniswav <azellaxmc@gmail.com>
2024-01-25 21:01:01 +0000

    Updated the config loader to add the functionality of creating a list of cogs.

--- tools/config_loader.py
+++ tools/config_loader.py
@@ -1,16 +1,20 @@
 import configparser
 import os
 
-class ConfigLoader:
+class ConfigLoader(configparser.ConfigParser):
     def __init__(self, *file_names):
-        self.config = configparser.ConfigParser()
+        super().__init__()
         for file_name in file_names:
             file_path = os.path.join(os.path.dirname(__file__), '..', 'config', file_name)
             if os.path.exists(file_path):
-                self.config.read(file_path)
+                self.read(file_path)
 
-    def get(self, section, key, fallback=None):
-        return self.config.get(section, key, fallback=fallback)
+    def getlist(self, section, option):
+        value = self.get(section, option)
+        return list(filter(None, (x.strip() for x in value.splitlines())))
 
-# Initialization with configuration file names
+    def getlistint(self, section, option):
+        return [int(x) for x in self.getlist(section, option)]
+
+# Initialisation avec les noms des fichiers de configuration
 config_loader = ConfigLoader('bot.ini', 'tokens.ini')
